253cb2b527a9961354a0cd3421d17f43bd30ab21
vindafadilla <vinda.fadilla.tif14@polban.ac.id>
2017-05-17 09:34:07 +0700

    third commit

---
+++ ta_backend/datapreprocessing/datapreprocessingcalculation.py
@@ -0,0 +1,78 @@
+from __future__ import division, print_function, absolute_import
+
+import argparse
+import sys
+import logging
+
+from ta_backend_skeleton import __version__
+
+__author__ = "vindafadilla"
+__copyright__ = "vindafadilla"
+__license__ = "none"
+
+_logger = logging.getLogger(__name__)
+
+
+def fib(n):
+    """Fibonacci example function
+
+    Args:
+      n (int): integer
+
+    Returns:
+      int: n-th Fibonacci number
+    """
+    assert n > 0
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Just a Fibonnaci demonstration")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='ta_backend_skeleton {ver}'.format(ver=__version__))
+    parser.add_argument(
+        dest="n",
+        help="n-th Fibonacci number",
+        type=int,
+        metavar="INT")
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
